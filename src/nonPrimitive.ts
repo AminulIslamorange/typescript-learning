6dba39b309bc4ca40067d2f99516c9688b4961f5
AminulIslamorange <aminulislamorange@gmail.com>
2026-05-01 18:57:54 +0600

    nonPrimitive added

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,16 @@
+// Array,object
+// tuple
+
+
+
+let bazarList :string[]=['milk','egg','potato','suger'];
+
+
+bazarList.push()
+
+let mixedArray : (string | number)[] =['egg',12, 'milk',1, 'suger',2];
+
+mixedArray.push(12)
+
+
+let couple:[string,string]=['husband','wife']
--- src/primitive.ts
+++ src/primitive.ts
@@ -6,4 +6,3 @@
 let username:string='aminul';
 
 username='orange'
-username=123
